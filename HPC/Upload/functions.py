19425480d0f39e1d36284cf455ac51a6540dec77
Dustin S <dustin.seboldt@fau.de>
2025-04-03 17:33:59 +0200

    warte noch auf 2 simulationen

--- HPC/Upload/functions.py
+++ HPC/Upload/functions.py
@@ -1024,8 +1024,8 @@ def qec_ft(qc: QuantumCircuit, qecc, pos: int):         #70 gates, 72 depth
 
 ############################################################################################################################################clsls####################
 def gen_data(name):
-    x = np.linspace(0.001,0.002,3)
-    shots = 10
+    x = np.linspace(0.000,0.0005,2)
+    shots = 20
     one, zero, one_QEC, zero_QEC, pre, post, pre_QEC, post_QEC = [],[],[],[],[],[],[],[]
     for i in x:
         qc = code_goto()
@@ -1063,4 +1063,4 @@ def gen_data(name):
         pre_QEC.append(preselec), post_QEC.append(postselec), one_QEC.append(ones), zero_QEC.append(zeros)
 
     data = np.array((x,pre,post,zero,one,pre_QEC,post_QEC, zero_QEC, one_QEC))
-    np.savetxt("FTSteane_3rd_n{}.txt".format(name), data, delimiter=",")
+    np.savetxt("FTSteane_3rd_n+{}.txt".format(name), data, delimiter=",")
